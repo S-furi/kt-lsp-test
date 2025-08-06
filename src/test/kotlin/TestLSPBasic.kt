6c88d95bf6b1b160c83861cc8a1695d6f1744cb2
Stefano Furi <stefano.furi@jetbrains.com>
2025-08-06 10:04:52 +0200

    refactor: make use of dataclasses and JSON-RPC modukle

--- src/test/kotlin/TestLSPBasic.kt
+++ src/test/kotlin/TestLSPBasic.kt
@@ -1,43 +1,18 @@
 @file:OptIn(ExperimentalSerializationApi::class)
 
-import kotlinx.serialization.EncodeDefault
 import kotlinx.serialization.ExperimentalSerializationApi
-import kotlinx.serialization.Serializable
 import org.junit.jupiter.api.Test
 import java.net.Socket
-import kotlinx.serialization.json.Json
-import kotlin.collections.emptyMap
 import kotlin.test.assertTrue
 
 class TestLSPBasic {
 
-    @Serializable
-    data class InitializeMessage(
-        @EncodeDefault val jsonrpc: String = "2.0",
-        @EncodeDefault val id: Int = 1,
-        @EncodeDefault val method: String = "initialize",
-        @EncodeDefault val params: Map<String, Map<String, String?>?> = mapOf(
-            "processId" to null,
-            "workspaceFolders" to null,
-            "capabilities" to emptyMap()
-        )
-    )
-
-    fun buildJSONRPCMessage(msg: InitializeMessage): ByteArray {
-        val body = Json.encodeToString(msg);
-        return "Content-Length: ${body.length}\r\n\r\n$body".toByteArray(Charsets.UTF_8)
-    }
-
     @Test
     fun testInitialisation() {
-        val initializeMessage = buildJSONRPCMessage(InitializeMessage())
+        val initializeMessage = JsonRpcUtils.buildJSONRPCMessage(InitializeMessage())
         Socket("127.0.0.1", 9999).use {client ->
             client.outputStream.write(initializeMessage)
-
-            val buffer = ByteArray(4096)
-            val bytesRead = client.getInputStream().read(buffer)
-            val response = String(buffer, 0, bytesRead, Charsets.UTF_8)
-
+            val response = JsonRpcUtils.readRPCResponse(client.inputStream)
             assertTrue { response.isNotEmpty() }
             println(response)
         }
